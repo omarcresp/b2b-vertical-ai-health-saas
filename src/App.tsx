ff01aca1546326799625930b56a13c95b4c02ad2
Omar Crespo <crespomerchano@gmail.com>
2026-02-21 17:37:13 -0500

    deployment

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,13 @@ import { api } from "../convex/_generated/api";
 export default function App() {
   return (
     <>
-      <header className="sticky top-0 z-10 bg-light dark:bg-dark p-4 border-b-2 border-slate-200 dark:border-slate-800 flex flex-row justify-between items-center">
-        Convex + React + WorkOS AuthKit
+      <header>
+        Convex + React + WorkOS AuthKit + Shadcn
         <AuthButton />
       </header>
       <main className="p-8 flex flex-col gap-16">
         <h1 className="text-4xl font-bold text-center">
-          Convex + React + WorkOS AuthKit
+          Convex + React + WorkOS AuthKit + Shadcn
         </h1>
         <Authenticated>
           <Content />
